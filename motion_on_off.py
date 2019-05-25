acb08564e7f1ff1b4edaae3e1cca4f3d87c5772e
kostq <admin@kostq.ru>
2019-05-25 14:19:03 +0300

    +

--- motion_on_off.py
+++ motion_on_off.py
@@ -10,7 +10,7 @@ def motion_on_off():
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=ip,username=user,password=password,look_for_keys=False,allow_agent=False)
     stdin,stdout,stderr = client.exec_command('sudo python3 /home/kostq/projects/home/check_motion.py')
-    stdin.write(password'\n')
+    stdin.write(password+"\n")
     result = stdout.read().decode('ascii').strip("\n")
     if result == 'on':
         print('OK')
