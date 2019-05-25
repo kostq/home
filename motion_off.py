da110577ba1a79df1ed3415b69640132bbcaadc0
kostq <kostq10@gmail.com>
2019-05-25 15:45:53 +0300

    +++

--- motion_off.py
+++ motion_off.py
@@ -1,5 +1,6 @@
 import io
 import sys
+import os
 
 def motion_off():
     with open('/etc/motioneye/thread-2.conf','r') as file:
@@ -7,5 +8,5 @@ def motion_off():
     data[5] = '# @motion_detection off' + '\n'
     with open('/etc/motioneye/thread-2.conf','w') as file:
         file.writelines(data)
-
+    command = os.system('echo kochergin2112955 | sudo -S systemctl restart motioneye')
 motion_off()
--- motion_on.py
+++ motion_on.py
@@ -1,11 +1,14 @@
 import io
+import os
 import sys
+import subprocess
 
 def motion_on():
+    password = 'kochergin2112955'
     with open('/etc/motioneye/thread-2.conf','r') as file:
         data = file.readlines()
     data[5] = '# @motion_detection on' + '\n'
     with open('/etc/motioneye/thread-2.conf','w') as file:
         file.writelines(data)
-
+    command = os.system('echo kochergin2112955 | sudo -S systemctl restart motioneye')
 motion_on()
