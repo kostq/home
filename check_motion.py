796846c25aa5a1da1a2c0d7ccc18c9221350be5d
kostq <admin@kostq.ru>
2019-05-25 13:10:31 +0300

    +++

--- check_motion.py
+++ check_motion.py
@@ -5,8 +5,8 @@ def check_status():
     with open('/etc/motioneye/thread-2.conf') as file:
         data = file.readlines()
     if 'motion_detection on' in data[5]:
-        return 'on'
+        print 'on'
     else:
-        return 'off'
+        print 'off'
 
 check_status()
