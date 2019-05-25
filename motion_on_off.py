ac00293deee0df0b68ab9c7bfab05c5802cf41bc
kostq <admin@kostq.ru>
2019-05-25 13:46:11 +0300

    add motion_on_off

---
+++ motion_on_off.py
@@ -0,0 +1,16 @@
+import paramiko
+import io
+import sys
+
+def motion_on_off():
+    user = 'kostq'
+    password = 'kochergin1'
+    ip = '192.168.1.16'
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=ip,username=user,password=password,look_for_keys=False,allow_agent=False)
+    stdin,stdout,stderr = client.exec_command('python3 /home/kostq/projects/home/check_motion.py')
+    print (stdout.read())
+    client.close()
+
+motion_on_off()
