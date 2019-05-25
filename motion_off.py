100e24837c1f47a8a87bcda13e4f354c184b9b9f
kostq <admin@kostq.ru>
2019-05-25 14:47:37 +0300

    +

--- motion_off.py
+++ motion_off.py
@@ -2,6 +2,8 @@ import io
 import sys
 
 def off():
+    with open('/etc/motioneye/thread-2.conf') as file:
+        data = file.readlines()
     data[5] = '# @motion_detection off'
     with open('/etc/motioneye/thread-2.conf') as file:
         file.writelines(data)
--- motion_on.py
+++ motion_on.py
@@ -2,6 +2,8 @@ import io
 import sys
 
 def on():
+    with open('/etc/motioneye/thread-2.conf') as file:
+        data = file.readlines()
     data[5] = '# @motion_detection on'
     with open('/etc/motioneye/thread-2.conf') as file:
         file.writelines(data)
