aa8a4b1c52e842a1d293c116b4b913db7ffea9c9
kostq <admin@kostq.ru>
2019-05-25 16:43:27 +0300

    +++

--- motion_on_off.py
+++ motion_on_off.py
@@ -11,7 +11,7 @@ def check():
     client.connect(hostname=ip,username=user,password=password,look_for_keys=False,allow_agent=False)
     stdin,stdout,stderr = client.exec_command('python3 /home/kostq/projects/home/check_motion.py')
     result = stdout.read().decode('ascii').strip("\n")
-    print(result)
+    return result
     client.close()
 
 check()
